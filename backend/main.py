f332d797dddccb507bf127884a6963a25dbdb061
amri-tah <amrithanandini2003@gmail.com>
2024-07-14 19:47:11 +0530

    testing for deployment

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this as needed
+    allow_origins=[ "https://mix-n-match-studio-myntra.vercel.app/","http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
